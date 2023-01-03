cfaffb55bd39b614056da686f6100cfdb553cc04
Aurelio Capingana <aurelio.capingana01@gmail.com>
2023-01-02 23:58:24 -0300

    Aprendendo funcoes  jS

---
+++ B7/03-BasicoJS/index04.js
@@ -0,0 +1,30 @@
+
+/* Calcular o preco do imovel */
+
+function CalcularImovel(metragem, quartos) {
+    let m2 = 3000
+    let  preco = 0
+    
+    switch (quartos) {
+        case 1:
+            preco = metragem * m2
+            break;
+        case 2:
+            preco = metragem * (m2 * 1.2)
+            break;
+        case 3:
+            preco = metragem * (m2 * 1.5)
+            break;
+        default:
+            if(preco = 4) {
+                preco = undefined
+            }
+            break;
+    }
+    return preco
+}
+
+let metragem = 123
+let quartos = 3
+let preco = CalcularImovel(metragem, quartos)
+console.log(`A casa custa R$ ${preco}`)
