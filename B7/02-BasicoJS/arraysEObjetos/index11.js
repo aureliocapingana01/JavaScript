884f57451aeae8e317129299d5dd137fac26aba7
Aurelio Capingana <aurelio.capingana01@gmail.com>
2023-01-22 16:36:10 -0300

    Termino do capitulo do JS basico

--- B7/02-BasicoJS/arraysEObjetos/index11.js
+++ B7/02-BasicoJS/arraysEObjetos/index11.js
@@ -13,8 +13,30 @@ console.log(nationality)
 
 */
 
-let nationality = country.every((value) => {
-    return value.length > 7
+/*
 
+
+let nationality = country.every((value) => {
+    return value.length > 5
+// o every retorna verdadeira se todos os itens do array satisfazerem a condicao 
 })
-console.log(nationality)
+if(nationality) {
+    console.log(`A nacionalidade é ${nationality}`)
+}
+else{
+    console.log('ERRO')
+}
+
+*/
+
+
+// o some retorna verdadeira se alguns itens satisfazerem a codicao 
+
+
+if(country.includes('Angola')) {
+    console.log(`Você é de nacioanlidade`)
+}
+else {
+    console.log('nao tem teu pais')
+}
+
